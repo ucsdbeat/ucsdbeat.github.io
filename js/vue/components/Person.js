e7a22786b0202502b7041c8ada89688a3d3975cb
Andrew Mokhtarzadeh <amokhtarzadeh22@gmail.com>
2019-09-11 13:39:25 -0700

    about member data/bg pics

--- js/vue/components/Person.js
+++ js/vue/components/Person.js
@@ -1,5 +1,5 @@
 const Person = Vue.component('person', {
-    props: ['first', 'last', 'part', 'picture', 'link', 'socla1', 'soli1', 'socla2', 'soli2'],
+    props: ['first', 'last', 'part', 'picture', 'link', 'socla1', 'soli1', 'socla2', 'soli2', 'socla3', 'soli3', 'socla4', 'soli4'],
     template: `
     <div class="col-lg-3 col-md-4 col-sm-6">
     <div class="speaker-item">
@@ -10,6 +10,9 @@ const Person = Vue.component('person', {
                 <ul class="list-inline">
                     <li class="list-inline-item"><a v-bind:href="soli1"><i v-bind:class="socla1"></i></a></li>
                     <li class="list-inline-item"><a v-bind:href="soli2"><i v-bind:class="socla2"></i></a></li>
+                    <li class="list-inline-item"><a v-bind:href="soli3"><i v-bind:class="socla3"></i></a></li>
+                    <li class="list-inline-item"><a v-bind:href="soli4"><i v-bind:class="socla4"></i></a></li>
+
                 </ul>
             </div>
         </div>
--- js/vue/members.js
+++ js/vue/members.js
@@ -15,7 +15,15 @@ const app = new Vue({
               },
               {
                 class: 'fa fa-instagram',
-                link: 'ewe'
+                link: 'https://www.instagram.com/icklefinniekins/?hl=en'
+              },
+              {
+                class: 'fa fa-linkedin',
+                link: 'https://www.linkedin.com/in/finiva-ednalino-081110129/'
+              },
+              {
+                class: 'fa fa-youtube',
+                link: 'https://www.youtube.com/user/theucsdbeat'
               }
             ]
           },
@@ -32,8 +40,266 @@ const app = new Vue({
               },
               {
                 class: 'fa fa-instagram',
-                link: 'h079111'
+                link: 'https://www.instagram.com/monkeytarzan/?hl=en'
               },
+              {
+                class: 'fa fa-linkedin',
+                link: 'https://www.linkedin.com/in/andrewmokht/'
+              },
+              {
+                class: 'fa fa-youtube',
+                link: 'https://www.youtube.com/user/theucsdbeat'
+              }
+            ]
+          },
+          {
+            firstName : 'Jacob',
+            lastName : 'Benson',
+            image: './images/members/jac.jpg',
+            partPos: 'Music Director - Baritone',
+            link: './members/jacob.html',
+            socials: [
+              {
+                class: 'fa fa-facebook',
+                link: 'https://www.facebook.com/profile.php?id=100013713079111'
+              },
+              {
+                class: 'fa fa-instagram',
+                link: 'https://www.instagram.com/monkeytarzan/?hl=en'
+              },
+              {
+                class: 'fa fa-linkedin',
+                link: 'https://www.linkedin.com/in/andrewmokht/'
+              },
+              {
+                class: 'fa fa-youtube',
+                link: 'https://www.youtube.com/user/theucsdbeat'
+              }
+            ]
+          },
+          {
+            firstName : 'Anu',
+            lastName : 'Sethuraman',
+            image: './images/members/anu.jpg',
+            partPos: 'Music Director - Alto',
+            link: './members/anu.html',
+            socials: [
+              {
+                class: 'fa fa-facebook',
+                link: 'https://www.facebook.com/profile.php?id=100013713079111'
+              },
+              {
+                class: 'fa fa-instagram',
+                link: 'https://www.instagram.com/monkeytarzan/?hl=en'
+              },
+              {
+                class: 'fa fa-linkedin',
+                link: 'https://www.linkedin.com/in/andrewmokht/'
+              },
+              {
+                class: 'fa fa-youtube',
+                link: 'https://www.youtube.com/user/theucsdbeat'
+              }
+            ]
+          },
+          {
+            firstName : 'David',
+            lastName : 'Chiu',
+            image: './images/members/dav.jpg',
+            partPos: 'Public Relations - Bass',
+            link: './members/david.html',
+            socials: [
+              {
+                class: 'fa fa-facebook',
+                link: 'https://www.facebook.com/profile.php?id=100013713079111'
+              },
+              {
+                class: 'fa fa-instagram',
+                link: 'https://www.instagram.com/monkeytarzan/?hl=en'
+              },
+              {
+                class: 'fa fa-linkedin',
+                link: 'https://www.linkedin.com/in/andrewmokht/'
+              },
+              {
+                class: 'fa fa-youtube',
+                link: 'https://www.youtube.com/user/theucsdbeat'
+              }
+            ]
+          },
+          {
+            firstName : 'Catherine',
+            lastName : 'Kuh',
+            image: './images/members/cat.jpg',
+            partPos: 'Public Relations - Alto',
+            link: './members/cat.html',
+            socials: [
+              {
+                class: 'fa fa-facebook',
+                link: 'https://www.facebook.com/profile.php?id=100013713079111'
+              },
+              {
+                class: 'fa fa-instagram',
+                link: 'https://www.instagram.com/monkeytarzan/?hl=en'
+              },
+              {
+                class: 'fa fa-linkedin',
+                link: 'https://www.linkedin.com/in/andrewmokht/'
+              },
+              {
+                class: 'fa fa-youtube',
+                link: 'https://www.youtube.com/user/theucsdbeat'
+              }
+            ]
+          },
+          {
+            firstName : 'Mika',
+            lastName : 'Carl',
+            image: './images/members/mik.jpg',
+            partPos: 'Treasurer - Soprano',
+            link: './members/mika.html',
+            socials: [
+              {
+                class: 'fa fa-facebook',
+                link: 'https://www.facebook.com/profile.php?id=100013713079111'
+              },
+              {
+                class: 'fa fa-instagram',
+                link: 'https://www.instagram.com/monkeytarzan/?hl=en'
+              },
+              {
+                class: 'fa fa-linkedin',
+                link: 'https://www.linkedin.com/in/andrewmokht/'
+              },
+              {
+                class: 'fa fa-youtube',
+                link: 'https://www.youtube.com/user/theucsdbeat'
+              }
+            ]
+          },
+          {
+            firstName : 'Katelyn',
+            lastName : 'Benson',
+            image: './images/members/kat.jpg',
+            partPos: 'Soprano',
+            link: './members/katelyn.html',
+            socials: [
+              {
+                class: 'fa fa-facebook',
+                link: 'https://www.facebook.com/profile.php?id=100013713079111'
+              },
+              {
+                class: 'fa fa-instagram',
+                link: 'https://www.instagram.com/monkeytarzan/?hl=en'
+              },
+              {
+                class: 'fa fa-linkedin',
+                link: 'https://www.linkedin.com/in/andrewmokht/'
+              },
+              {
+                class: 'fa fa-youtube',
+                link: 'https://www.youtube.com/user/theucsdbeat'
+              }
+            ]
+          },
+          {
+            firstName : 'Sarah',
+            lastName : 'Austin',
+            image: './images/members/sar.jpg',
+            partPos: 'Alto',
+            link: './members/sarah.html',
+            socials: [
+              {
+                class: 'fa fa-facebook',
+                link: 'https://www.facebook.com/profile.php?id=100013713079111'
+              },
+              {
+                class: 'fa fa-instagram',
+                link: 'https://www.instagram.com/monkeytarzan/?hl=en'
+              },
+              {
+                class: 'fa fa-linkedin',
+                link: 'https://www.linkedin.com/in/andrewmokht/'
+              },
+              {
+                class: 'fa fa-youtube',
+                link: 'https://www.youtube.com/user/theucsdbeat'
+              }
+            ]
+          },
+          {
+            firstName : 'Christopher',
+            lastName : 'Kagoo',
+            image: './images/members/chr.jpg',
+            partPos: 'Tenor',
+            link: './members/chris.html',
+            socials: [
+              {
+                class: 'fa fa-facebook',
+                link: 'https://www.facebook.com/profile.php?id=100013713079111'
+              },
+              {
+                class: 'fa fa-instagram',
+                link: 'https://www.instagram.com/monkeytarzan/?hl=en'
+              },
+              {
+                class: 'fa fa-linkedin',
+                link: 'https://www.linkedin.com/in/andrewmokht/'
+              },
+              {
+                class: 'fa fa-youtube',
+                link: 'https://www.youtube.com/user/theucsdbeat'
+              }
+            ]
+          },
+          {
+            firstName : 'Jae',
+            lastName : 'Lee',
+            image: './images/members/jae.jpg',
+            partPos: 'Bass/Vocal Percussion',
+            link: './members/jae.html',
+            socials: [
+              {
+                class: 'fa fa-facebook',
+                link: 'https://www.facebook.com/profile.php?id=100013713079111'
+              },
+              {
+                class: 'fa fa-instagram',
+                link: 'https://www.instagram.com/monkeytarzan/?hl=en'
+              },
+              {
+                class: 'fa fa-linkedin',
+                link: 'https://www.linkedin.com/in/andrewmokht/'
+              },
+              {
+                class: 'fa fa-youtube',
+                link: 'https://www.youtube.com/user/theucsdbeat'
+              }
+            ]
+          },
+          {
+            firstName : 'Johnathan',
+            lastName : 'Chang',
+            image: './images/members/joh.jpg',
+            partPos: 'Bass',
+            link: './members/johnny.html',
+            socials: [
+              {
+                class: 'fa fa-facebook',
+                link: 'https://www.facebook.com/profile.php?id=100013713079111'
+              },
+              {
+                class: 'fa fa-instagram',
+                link: 'https://www.instagram.com/monkeytarzan/?hl=en'
+              },
+              {
+                class: 'fa fa-linkedin',
+                link: 'https://www.linkedin.com/in/andrewmokht/'
+              },
+              {
+                class: 'fa fa-youtube',
+                link: 'https://www.youtube.com/user/theucsdbeat'
+              }
             ]
           }
 
