74c3f2b3273b2449b61460f98fe7d87328ccb213
Aliff <aliff.macapinlac@gmail.com>
2019-09-17 14:41:22 -0700

    more navbar stuff

--- js/main.js
+++ js/main.js
@@ -9,17 +9,23 @@ console.log(nav);
 
 window.addEventListener('scroll', function(){
     let links = (nav.querySelectorAll('.nav-link.nav-link-white').length > 0) ? nav.querySelectorAll('.nav-link.nav-link-white') : nav.querySelectorAll('.nav-link.nav-link-black');
-    console.log(links);
     if(window.scrollY > 50) {
-        loopThrough(links, 'nav-link-black', 'nav-link-white');
+        loopThroughAndChangeFont(links, 'nav-link-black', 'nav-link-white');
+        changeBackgoundColor(nav, 'main-nav-white', 'main-nav-transparent');
     } else {
-        loopThrough(links, 'nav-link-white', 'nav-link-black');
+        loopThroughAndChangeFont(links, 'nav-link-white', 'nav-link-black');
+        changeBackgoundColor(nav, 'main-nav-transparent', 'main-nav-white');
     }
 })
 
-function loopThrough(links, addClassName, removeClassName) {
+function loopThroughAndChangeFont(links, addClassName, removeClassName) {
     for (let i = 0; i < links.length; i++) {
         links[i].classList.add(addClassName);
         links[i].classList.remove(removeClassName);
     }     
+}
+
+function changeBackgoundColor (nav, addName, removeName) { 
+    nav.classList.add(addName)
+    nav.classList.remove(removeName)
 }
