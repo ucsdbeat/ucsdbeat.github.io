bdd47c49ace9e11ca41eead8df8edf988e1376a1
Andrew Mok <amokhtarzadeh22@gmail.com>
2022-02-01 01:16:23 -0800

    re-populated performances

--- js/vue/shows.js
+++ js/vue/shows.js
@@ -3,12 +3,12 @@ const app = new Vue({
     data: {
         shows: [
           {
-            title: "God Is A Woman",
-            location: "2019 Spring Concert",
+            title: "ICCA 2020 Set",
+            location: "ICCA Southwest Quarterfinals 2020",
             link: "https://www.youtube.com/watch?v=SH228rEfS_w",
             day: "27",
             month: "Mar",
-            picture: "./images/performances/giaw.jpg"
+            picture: "./images/performances/icca2020.jpg"
           },
           {
             title: "God Is A Woman",
@@ -17,6 +17,38 @@ const app = new Vue({
             day: "27",
             month: "Mar",
             picture: "./images/performances/giaw.jpg"
+          },
+          {
+            title: "Hey There Delilah",
+            location: "2019 Spring Concert",
+            link: "https://www.youtube.com/watch?v=8sNmYMaIbjc",
+            day: "27",
+            month: "Mar",
+            picture: "./images/performances/htd.jpg"
+          },
+          {
+            title: "Don't You Worry Bout A Thing",
+            location: "2019 Spring Concert",
+            link: "https://www.youtube.com/watch?v=G-4d5xbhZ4A",
+            day: "27",
+            month: "Mar",
+            picture: "./images/performances/dywbat.jpg"
+          },
+          {
+            title: "Turning Tables",
+            location: "2019 Spring Concert",
+            link: "https://www.youtube.com/watch?v=PTSHFRfFuRY",
+            day: "27",
+            month: "Mar",
+            picture: "./images/performances/tt.jpg"
+          },
+          {
+            title: "Used to Love U++",
+            location: "2019 Spring Concert",
+            link: "https://www.youtube.com/watch?v=tm9LhR8NanY",
+            day: "27",
+            month: "Mar",
+            picture: "./images/performances/utlu.jpg"
           }
 
       ]
